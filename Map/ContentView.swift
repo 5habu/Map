1795e46f4271b654a694e391b4950e9d2c8dab0d
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-30 23:29:46 +0900

    Initial Commit

---
+++ Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by 5habu on 2022/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by 5habu on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
